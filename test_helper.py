1313023a9e381eca007e8b756cd371b787c70980
Neox420 <niels9875@gmail.com>
2023-09-08 14:06:07 +0200

    #1

--- test_helper.py
+++ test_helper.py
@@ -2,21 +2,24 @@ import datetime
 import pytest
 import helper
 
-def test_sort(): 
 
+def test_sort():
     # Given: I have several to-dos with dates
-    todos = [("Universum debuggen", "2023-09-06"), 
-             ("Sinn des Lebens entdecken", "2023-09-01"), 
-             ("Superheld werden", "2023-10-25"), 
-             ("Netto null", "2050-01-01")] 
+    todos = [
+        ("Universum debuggen", "2023-09-06"),
+        ("Sinn des Lebens entdecken", "2023-09-01"),
+        ("Superheld werden", "2023-10-25"),
+        ("Netto null", "2050-01-01"),
+    ]
 
-    # When: I add the items 
-    for todo in todos: 
-        helper.add(todo[0], todo[1])  
+    # When: I add the items
+    for todo in todos:
+        helper.add(todo[0], todo[1])
 
-    # Then: They should be sorted by date 
-    for i in range(len(helper.items) - 1): 
-        assert helper.items[i].date < helper.items[i+1].date 
+    # Then: They should be sorted by date
+    for i in range(len(helper.items) - 1):
+        assert helper.items[i].date < helper.items[i + 1].date
+
 
 def test_add():
     # Given: I want to add a to-do with a date
